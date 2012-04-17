edfd09c292e544f3ff9ad0a3f22e79fd29bb57a0
Dan Lidral-Porter <dlp@aperiodic.org>
2012-04-16 23:03:21 -0700

    Add github page to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject bifocals "0.0.1"
   :description "A kinect library for quil."
+  :url "http://github.com/aperiodic/bifocals"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.processing-core/org.processing.core "1.5.1"]
                  [simple-open-ni "0.26.0"]]
