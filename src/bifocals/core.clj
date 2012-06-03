a8f758e020c4f706a5d8bd499107787d02b7899a
Dan Lidral-Porter <dlp@aperiodic.org>
2012-06-03 15:35:30 -0700

    No need to call setup, add toggle-mirrored.
    The kinect object is now initialized when it is first needed, so there
    is no need to call `bifocals.core/setup` before using other functions.
    However, you can continue to call it, in case you don't use it right
    away and want to avoid a long delay in the middle of your sketch, while
    the kinect initializes.
    
    Add a function in core to toggle whether the kinect's depth and scene
    images are mirrored.

--- src/bifocals/core.clj
+++ src/bifocals/core.clj
@@ -3,9 +3,12 @@
   (:require [bifocals.event :as event]
             [bifocals.skeleton :as skel]))
 
-;; atoms
+;;
+;; Atoms
+;;
 
-(def kinect (atom nil))
+(def ^{:private true} kinect-atom
+  (atom nil))
 
 (def users
   "Contains the uids of all on-screen users."
@@ -17,53 +20,66 @@
   coordinates (that is, a map with :x, :y, and :z keys)."
   (atom {}))
 
-
-;; event handlers
+;;
+;; Event Handlers
+;;
 
 (defn- track-or-retry
   [_ uid success]
   (if success
-    (.startTrackingSkeleton @kinect uid)
-    (.requestCalibrationSkeleton @kinect uid true)))
+    (.startTrackingSkeleton @kinect-atom uid)
+    (.requestCalibrationSkeleton @kinect-atom uid true)))
 
 (defn- add-user
   [_ uid]
   (swap! users conj uid)
-  (.requestCalibrationSkeleton @kinect uid true))
+  (.requestCalibrationSkeleton @kinect-atom uid true))
 
 (defn- remove-user
   [_ uid]
   (swap! users disj uid)
   (swap! skeletons dissoc uid))
 
+;;
+;; Setup
+;;
 
-;; main api
+(defn setup []
+  "This is called to initialize the kinect object the first time it is needed,
+  but you can also call it in your setup to get the long initialization delay
+  out of the way."
+  (let [kinect (or @kinect-atom (new Ben))]
+    (when-not (.enableDepth kinect)
+      (throw (new Error (str "Couldn't set up the kinect object! Are you sure "
+                             "it's plugged in to both your computer and "
+                             "power?"))))
+    (when-not @kinect-atom
+      (swap! kinect-atom (constantly kinect)))
+    (.setMirror kinect true)
+    (.enableUser kinect Ben/SKEL_PROFILE_ALL)
+    (event/register kinect :new-user add-user)
+    (event/register kinect :lost-user remove-user)
+    (event/register kinect :end-calibration track-or-retry)
+    kinect))
 
-(defn setup
-  "This should be the first thing you call, before any other function in this
-  namespace, or there will be NPEs. By default, initializes the kinect in mirror
-  mode; pass `:mirror false` to disable this."
-  [& {:keys [mirror] :or {mirror true}}]
-  (let [kinect-obj (new Ben)]
-    (swap! kinect (fn [_] kinect-obj)))
-  (when-not (.enableDepth @kinect)
-    (throw (new Error (str "Couldn't set up the kinect object! Are you sure "
-                           "it's plugged in to both your computer and "
-                           "power?"))))
-  (when mirror
-    (.setMirror @kinect true))
-  (.enableUser @kinect Ben/SKEL_PROFILE_ALL)
-  (event/register @kinect :new-user add-user)
-  (event/register @kinect :lost-user remove-user)
-  (event/register @kinect :end-calibration track-or-retry))
+;;
+;; Main API
+;;
+
+(defn kinect []
+  "Returns the current kinect object, initializing it if necessary. It is
+  unlikely that you'll need to call this function."
+  (if-let [kinect @kinect-atom]
+    kinect
+    (setup)))
 
 (defn tick []
   "This must be called in your draw method, or else the depth and scene images
   will be all black, and user/skeleton tracking will not happen."
-  (.update @kinect)
+  (.update (kinect))
   (doseq [uid @users]
-    (when (.isTrackingSkeleton @kinect uid)
-      (swap! skeletons assoc uid (skel/skeleton @kinect uid)))))
+    (when (.isTrackingSkeleton (kinect) uid)
+      (swap! skeletons assoc uid (skel/skeleton (kinect) uid)))))
 
 
 (defn depth-image []
@@ -71,36 +87,39 @@
   able to handle it no problem, but pure clojure consumers should brace
   themselves for interop and check out the PImage javadocs at
   http://processing.googlecode.com/svn/trunk/processing/build/javadoc/core/index.html"
-  (.depthImage @kinect))
+  (.depthImage (kinect)))
 
 (defn depth-width []
   "The width of the depth image, in pixels."
-  (.depthWidth @kinect))
+  (.depthWidth (kinect)))
 
 (defn depth-height []
   "The height of the depth image, in pixels."
-  (.depthHeight @kinect))
+  (.depthHeight (kinect)))
 
 (defn scene-image []
   "The scene image is from the kinect's color camera. It is a PImage, so quil
   will be able to handle it no problem, but pure clojure consumers should brace
   themselves for interop and check out the PImage javadocs at
   http://processing.googlecode.com/svn/trunk/processing/build/javadoc/core/index.html"
-  (.sceneImage @kinect))
+  (.sceneImage (kinect)))
 
 (defn scene-width []
   "The width of the scene image, in pixels."
-  (.sceneWidth @kinect))
+  (.sceneWidth (kinect)))
 
 (defn scene-height []
   "The height of the scene image, in pixels."
-  (.sceneHeight @kinect))
+  (.sceneHeight (kinect)))
 
+(defn toggle-mirrored []
+  "Toggle mirroring of the depth and scene images (they are mirrored by default)"
+  (.setMirror (kinect) (not (.mirror (kinect)))))
 
 (defn project
   "Projects 3D coordinates into depth image coordinates."
   [coords]
-  (skel/project @kinect coords))
+  (skel/project (kinect) coords))
 
 (defn project-skeleton
   "Projects the given skeleton from 3D coordinates into depth image coordinates.
