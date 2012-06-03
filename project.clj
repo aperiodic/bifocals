99c631af46e28f355c20e5cfcddd155811aeab7b
Dan Lidral-Porter <dlp@aperiodic.org>
2012-06-03 11:27:20 -0700

    Dep bump, put examples on lein's classpath.
    Updated the simple-open-ni dependency to 0.27.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject bifocals "0.0.1"
+(defproject bifocals "0.0.2"
   :description "A kinect library for quil."
   :url "http://github.com/aperiodic/bifocals"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.processing-core/org.processing.core "1.5.1"]
-                 [simple-open-ni "0.26.0"]]
+                 [simple-open-ni "0.27.0"]]
   :dev-dependencies [[quil "1.0.0"]]
-  :java-source-paths ["src/java/franklin"]
+  :extra-classpath-dirs ["examples"]
+  :java-source-path "src/java/franklin"
   :jar-exclusions [#"\.swp$" #"\.swo$" #"\.java"])
